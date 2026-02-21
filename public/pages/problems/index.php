e99ec2ca59c00183f99644f7241171acad22d259
André de Souza Aranda <andredesouzaaranda@gmail.com>
2026-02-21 14:14:02 -0300

    Layout Refactor

--- public/pages/problems/index.php
+++ public/pages/problems/index.php
@@ -1,6 +1,6 @@
 <?php
 
 $title = 'Problemas Registrados';
-$view = '/var/www/app/views/problems/new.phtml';
+$view = '/var/www/app/views/problems/index.phtml';
 
 require '/var/www/app/views/layouts/application.phtml';
