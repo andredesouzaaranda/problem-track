1c9cec80f92f3e03ab231cf29fb3c7964ca12fb1
André de Souza Aranda <andredesouzaaranda@gmail.com>
2026-03-02 23:09:24 -0300

    feat: Delete

--- public/pages/problems/index.php
+++ public/pages/problems/index.php
@@ -2,7 +2,9 @@
 
 define('DB_PATH', '/var/www/database/problems.txt');
 $problems = file(DB_PATH, FILE_IGNORE_NEW_LINES);
-
+if (empty($problems)) {
+  $problems = [];
+}
 $title = 'Problemas Registrados';
 $view = '/var/www/app/views/problems/index.phtml';
 
