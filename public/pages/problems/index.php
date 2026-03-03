19449ec9b58309708c740961c5c931c969a25294
André de Souza Aranda <andredesouzaaranda@gmail.com>
2026-03-02 21:40:29 -0300

    feat: Update (#11)

--- public/pages/problems/index.php
+++ public/pages/problems/index.php
@@ -1,10 +1,6 @@
 <?php
 
 define('DB_PATH', '/var/www/database/problems.txt');
-if (!is_dir(dirname(DB_PATH))) {
-  mkdir(dirname(DB_PATH), 0777, true);
-}
-
 $problems = file(DB_PATH, FILE_IGNORE_NEW_LINES);
 
 $title = 'Problemas Registrados';
